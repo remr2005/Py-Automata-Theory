59ac8ee520ddca78fc9f73560184b476446525f5
UltraUberTurk <morpeh2005ka@gmail.com>
2025-04-02 10:56:59 +0300

    Чут чут подправил

--- PyAutomataTheory/maximization/anger_pohl.py
+++ PyAutomataTheory/maximization/anger_pohl.py
@@ -4,56 +4,46 @@ from functools import lru_cache
 from copy import deepcopy
 from PyAutomataTheory.automatas import MealyAutomata
 
+way = set()
+
 def anger_pohl(automata:MealyAutomata) -> None:
     """Anger-Pohl algorithm"""
     aut = deepcopy(automata)
-    blocks = defaultdict(set)
-    binMatrix = {}
-
-    # for s0, a0 in aut.table.items():
-    #     for s1,a1 in aut.table.items():
-    #         if s0==s1:
-    #             continue
-            
-    #         Yav_Soot = True
-    #         # Нахождение явного/неявного соответствия
-    #         for inp in aut.alphabet:
-    #             if a0[inp][1] != a1[inp][1] and a0[inp][1] != "-" and a1[inp][1] != "-":
-    #                 binMatrix[tuple([min(s0, s1), max(s0, s1)])] = 0
-    #             for i in range(2):
-    #                 if not (a0[inp][i] == a1[inp][i] or a0[inp][i] == "-" or a1[inp][i] == "-"):
-    #                     Yav_Soot = False
-    #                     break
-        
-    #         # Эту надо отрефакторить
-    #         if Yav_Soot:
-    #             blocks[min(s0,s1)].add(max(s0, s1))
-    #             binMatrix[tuple([min(s0, s1), max(s0, s1)])] = 1           
-    
+    blocks_row = defaultdict(set)
+    blocks_table = defaultdict(set)
+    binMatrix = {}    
     for s0, a0 in aut.table.items():
         for s1,a1 in aut.table.items():
             if s0==s1:
                 continue
-            print(s0,s1)
-            c, r = calculate(s0, s1, aut)
+            min_s = min(s0,s1)
+            max_s = max(s0,s1)
+
+
+            c, r = calculate(min_s, max_s, aut)
+            global way
+            way = set()
             binMatrix[c] = r
+            if r == 0:
+                print(c)
             if r:
-                blocks[min(s0,s1)].add(max(s0, s1))
+                blocks_row[min_s].add(max_s)
+                blocks_table[max_s].add(min_s)
+    calculate.cache_clear()
+    # print(binMatrix)
+    # print(blocks_row)
+    # print(blocks_table)
     
-    print(binMatrix)
-    print(blocks)
-    # TODO: По сути, (в идеале) нужно сделать рекурентную под фунцкию, которая будет проходится по всем путям
-    # TODO: Выбрасываем лишние пары(наверное стоит, можно делать это просто по ходу(а, нет скорее всего нельзая))
-    # Так как наша пара
 
 def get_way(a0_, a1_):
-    print(a0_, a1_)
+    # print(a0_, a1_)
     a0 = deepcopy(a0_)
     a1 = deepcopy(a1_)
     for inp, res in a0.items():
         print(1)
         if res[0] != '-' and a1[inp][0]!='-':
             return tuple([min(res[0],a1[inp][0]), max(res[0],a1[inp][0])])
+
 
 @lru_cache()
 def calculate(s0_, s1_, aut_:MealyAutomata):
@@ -63,6 +53,11 @@ def calculate(s0_, s1_, aut_:MealyAutomata):
     a0 = aut.table[s0]
     a1 = aut.table[s1]
     Yav_Soot = True
+    global way
+    if tuple([min(s0, s1), max(s0, s1)]) in way:
+        return tuple([min(s0, s1), max(s0, s1)]), 1
+    else:
+        way.add(tuple([min(s0, s1), max(s0, s1)]))
     # Нахождение явного/неявного соответствия
     for inp in aut.alphabet:
         if a0[inp][1] != a1[inp][1] and a0[inp][1] != "-" and a1[inp][1] != "-":
@@ -73,10 +68,7 @@ def calculate(s0_, s1_, aut_:MealyAutomata):
                 break
     if Yav_Soot:
         return tuple([min(s0, s1), max(s0, s1)]), 1
-    
-    print(a0)
 
     coord = get_way(a0, a1)
-    print(f"SAD {coord} SAD")
 
     return tuple([min(s0, s1), max(s0, s1)]),calculate(*coord,aut)[1]
