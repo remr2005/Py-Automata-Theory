ac734e177de2f64031a972e4faba06aaed19e60f
UltraUberTurk <morpeh2005ka@gmail.com>
2025-04-04 09:03:27 +0300

    Вроде пропердолил

--- PyAutomataTheory/maximization/anger_pohl.py
+++ PyAutomataTheory/maximization/anger_pohl.py
@@ -46,12 +46,11 @@ def anger_pohl(automata:MealyAutomata) -> None:
     
 
 def get_way(a0_, a1_):
-    # print(a0_, a1_)
+    """Возвращает найденные переходы"""
     a0 = deepcopy(a0_)
     a1 = deepcopy(a1_)
     ans = []
     for inp, res in a0.items():
-        # print(1)
         if res[0] != '-' and a1[inp][0]!='-' and  res[0]!=a1[inp][0]:
             ans.append(tuple([min(res[0],a1[inp][0]), max(res[0],a1[inp][0])]))
     return ans
@@ -59,6 +58,7 @@ def get_way(a0_, a1_):
 
 @lru_cache()
 def calculate(s0_, s1_, aut_:MealyAutomata):
+    """Находим совместимость/несовместимость"""
     s0 = deepcopy(s0_)
     s1 = deepcopy(s1_)
     aut = deepcopy(aut_)
@@ -76,6 +76,7 @@ def calculate(s0_, s1_, aut_:MealyAutomata):
     if Yav_Soot:
         return tuple([min(s0, s1), max(s0, s1)]), 1
     
+    # Проверяем, не вернулись ли мы туда, где мы уже были
     global way
     if tuple([min(s0, s1), max(s0, s1)]) in way:
         return tuple([min(s0, s1), max(s0, s1)]), 1
@@ -84,33 +85,35 @@ def calculate(s0_, s1_, aut_:MealyAutomata):
 
     coord = get_way(a0, a1)
 
-    # print(tuple([min(s0, s1), max(s0, s1)]),"->",coord)
+    # проходимся по всем возможным переходам
     ans = []
-    # return tuple([min(s0, s1), max(s0, s1)]),calculate(*coord,aut)[1]
     for c in coord:
         ans.append(calculate(*c,aut)[1])
     return tuple([min(s0, s1), max(s0, s1)]), int(all(ans))
 
 
 def is_block(block: list, binMatrix: dict[tuple, int]):
+    """Находит блоки, из данного"""
+    # Находим несовместимые состояния
     incor = [i for i in combinations(block, 2) if binMatrix.get(i, 0) != 1]
     
+    # Если таких состояний нет, то стоп
     if not incor:
-        # print(block)
-        return [block]  # Возвращаем список, содержащий один корректный блок
+        return [block]
 
     max_blocks = set()
-
+    # Ищем все блоки, в которых нет несовместимых состояний
     for i in block:
-        temp = deepcopy(block)  # Копируем список
-        temp.remove(i)  # Удаляем элемент
-        new_blocks = is_block(temp, binMatrix)  # Рекурсивный вызов
+        temp = deepcopy(block)
+        temp.remove(i)
+        new_blocks = is_block(temp, binMatrix)
 
         for nb in new_blocks:
             if len(nb) > 0:
-                max_blocks.add(tuple(nb))  # Добавляем блок, если он такой же по размеру
-
+                max_blocks.add(tuple(nb))
     final_blocks = []
+
+    # Убираем все подмножества таких множеств
     for cb in max_blocks:
         if not any(set(cb).issubset(set(other)) and cb != other for other in max_blocks):
             final_blocks.append(cb)
